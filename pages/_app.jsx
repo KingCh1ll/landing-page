6498ffa13b84a0a9fd59268ba61d4ac37a6bd6ba
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-04-30 15:01:13 -0400

    🚀 | Proformance Optimization

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,49 +1,24 @@
 import { useState, useEffect } from "react";
-import Head from "next/head";
+import AOS from "aos";
 
 import "pace-js/themes/blue/pace-theme-minimal.css";
 import "aos/dist/aos.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/global.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/global.scss";
 
 import config from "../config";
 
 export default function Render({ Component, pageProps }) {
     useEffect(() => {
-        import("jquery/src/jquery.js");
         import("pace-js/pace.min.js");
-        import("aos/dist/aos.js");
-        import("smooth-scroll/dist/smooth-scroll.polyfills.js");
-        import("../public/js/handle");
+        import("bootstrap/dist/js/bootstrap");
+
+        AOS.init({
+            once: false,
+            startEvent: "load",
+            duration: "600",
+        });
     }, []);
 
-    return (
-        <>
-            <Head>
-                <title>{config.meta.title}</title>
-                <link rel="manifest" href={config.meta.manifest} />
-                <link rel="shortcut icon" type="image/x-icon" href={config.meta.logo} />
-                <meta property="description" content={config.meta.description} />
-                <meta property="keywords" content={config.meta.keywords.join(", ").toLowerCase()} />
-
-                <meta name="twitter:title" content={config.meta.title} />
-                <meta name="twitter:description" content={config.meta.description} />
-                <meta name="twitter:site" content={config.meta.site} />
-                <meta name="twitter:card" content="summary" />
-
-                <meta property="og:title" content={config.meta.title} />
-                <meta property="og:description" content={config.meta.description} />
-                <meta property="og:image" content={config.meta.logo} />
-
-                <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-                <link rel="canonical" href={config.meta.site} />
-
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
-                <link href="https://use.fontawesome.com/releases/v6.1.0/css/all.css" rel="stylesheet" />
-            </Head>
-            <body>
-                <Component {...pageProps} />
-            </body>
-        </>
-    );
+    return <Component {...pageProps} />;
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react';
+import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import Script from "next/script";
 import { HiOutlineNewspaper, HiOutlineLink, HiX, HiOutlineExclamation, HiOutlineFolder, HiOutlineMenuAlt2, HiOutlineFire, HiOutlineCode } from 'react-icons/hi';
 
 import config from '../config';
-import style from '../styles/home.module.css';
+import style from '../styles/home.module.scss';
 
 export default function Render({ user, cards, error }) {
     const [width, setWidth] = useState()
@@ -18,6 +19,28 @@ export default function Render({ user, cards, error }) {
 
     return (
         <>
+            <Head>
+                <title>{config.meta.title}</title>
+                <link rel="manifest" href={config.meta.manifest} />
+                <link rel="shortcut icon" type="image/x-icon" href={config.meta.logo} />
+                <meta property="description" content={config.meta.description} />
+                <meta property="keywords" content={config.meta.keywords.join(", ").toLowerCase()} />
+
+                <meta name="twitter:title" content={config.meta.title} />
+                <meta name="twitter:description" content={config.meta.description} />
+                <meta name="twitter:site" content={config.meta.site} />
+                <meta name="twitter:card" content="summary" />
+
+                <meta property="og:title" content={config.meta.title} />
+                <meta property="og:description" content={config.meta.description} />
+                <meta property="og:image" content={config.meta.logo} />
+
+                <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+                <link rel="canonical" href={config.meta.site} />
+
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
+                <link href="https://use.fontawesome.com/releases/v6.1.0/css/all.css" rel="stylesheet" />
+            </Head>
             <section className="container-sm" id="features" style={{ paddingTop: "5rem", maxWidth: "700px" }}>
                 <div style={{ background: "#000d2b", borderRadius: "15px", padding: "30px", fontFamily: "Rubik, sans-serif" }}>
                     <div style={{ paddingTop: "1rem", paddingBottom: "3rem" }}>

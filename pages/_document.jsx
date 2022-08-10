4b7447a4afbb6049a5edc9db03dbc33700c42619
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2022-08-10 09:15:48 -0400

    🏠 | Fixed Document Dark

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,4 +1,5 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document"
+import { ColorModeScript } from "@chakra-ui/react";
 
 export default class Document extends NextDocument {
   render() {
@@ -10,6 +11,7 @@ export default class Document extends NextDocument {
           <div style={{ position: "fixed", height: "100%", width: "100%", zIndex: "-1" }}>
             <img defer src="/images/blobs.svg" alt="Cool Blob Background" style={{ height: "100%", width: "100%", opacity: ".4", WebkitMaskImage: "linear-gradient(to top, transparent 0%, #fff 100%)", objectFit: "cover" }} />
           </div>
+          <ColorModeScript initialColorMode={"dark"} />
           <Main />
           <NextScript />
         </body>

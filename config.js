b4de1bf4989124cfc2b88bab33b5761cc0a8f0a8
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2023-01-14 16:48:37 -0500

    🚀 | Better Description

--- config.js
+++ config.js
@@ -1,22 +1,16 @@
 module.exports = {
   name: "KingCh1ll",
-  shortDesc: "I'm a passionate <strong>Software Engineer</strong>, making <strong>websites</strong> and <strong>Discord bots</strong>. I'm also a former <a href=\"https://roblox.com\"><strong>Roblox</strong></a> game dev.",
+  shortDesc: "Passionate Software Engineer & Website Developer, living in the United States.",
   logo: "/images/users/kingch1ll.webp",
   client_ID: "1027401268227997778",
   invites: {
     disping: "https://discord.com/oauth2/authorize?scope=bot%20applications.commands&client_id=884525761694933073&permissions=294074575990&redirect_uri=https%3A%2F%2Fwww.ch1ll.tk%2Fapi%2Fauth%2Fcallback%2Fdiscord",
     sparkv: "https://discord.com/oauth2/authorize?client_id=1027401268227997778&scope=bot+applications.commands&response_type=code&redirect_uri=https://www.sparkv.tk/api/callback&permissions=294074575990"
   },
-  sections: {
-    about: {
-      image: "/images/users/kingch1ll.webp",
-      text: "Hi, I'm KingCh1ll! I'm a passionate <strong>Software Engineer</strong>, living in the <strong>United States</strong>.<br/><br/>I love making <strong>websites</strong> and <strong>Discord bots</strong>. I also used to make games on the platform <a href=\"https://roblox.com\">Roblox</a>. <br/><br/><strong>I first started coding in 2019</strong>, when I learned about <strong>Roblox Studio</strong>. <strong>Roblox Studio</strong> is the way to make games on the <strong>Roblox platform</strong>.",
-    }
-  },
   meta: {
     title: "KingCh1ll",
     logo: "/favicon.ico",
-    description: "I'm a passionate <strong>Software Engineer</strong>, making <strong>websites</strong> and <strong>Discord bots</strong>. I'm also a former <a href=\"https://roblox.com\"><strong>Roblox</strong></a> game dev.",
+    description: "A passionate Software Engineer & Website Developer, living in the United States.",
     site: "https://www.ch1ll.tk/",
     keywords: [
       "KingCh1ll",

de64e2cfdc6841bd4f050b3a41091e41eb625470
KingCh1ll <77991729+KingCh1ll@users.noreply.github.com>
2023-06-25 13:37:51 -0400

    ✨ | Better Privacy Page

--- app/(legal)/privacy/page.jsx
+++ app/(legal)/privacy/page.jsx
@@ -6,7 +6,7 @@ import { Heading } from "@chakra-ui/react";
 import Navbar from "../../(components)/navbar";
 
 const terms = [{
-    text: 'This privacy policy outlines how we use your data, across all of Ch1llDev\'s websites and Discord bots. By using any of Ch1llDev\'s Services (the "services"), you agree that you have read and accepted both our privacy policy, and the <a href="/legal/tos">Terms of Service</a>. If you have any questions or concerns regarding Ch1llDev\'s policy, you can email Ch1llDev here: <a href="mailto:support@ch1ll.dev">support@ch1ll.dev</a>'
+    text: 'This privacy policy outlines how we use your data, across all of Ch1llDev\'s websites and Discord bots. By using any of Ch1llDev\'s Services (the "services"), you agree that you have read and accepted both our privacy policy, and the <a href="/legal/legal/tos">Terms of Service</a>. If you have any questions or concerns regarding Ch1llDev\'s policy, you can email Ch1llDev here: <a href="mailto:support@ch1ll.dev">support@ch1ll.dev</a>'
 }, {
     header: "1. ⚙️ General"
 }, {
@@ -18,24 +18,19 @@ const terms = [{
 }, {
     header: "3. 🔒 Security"
 }, {
-    text: "DisPing stores your data safely and securly in a Mongodb database."
+    text: "DisPing stores your data safely and securely in a Mongodb database."
 }, {
     header: "3. ⏰ Duration & Deletion"
 }, {
-    text: 'DisPing stores your data until it is no longer needed, or the terms of service has been terminated. If you would like your data to be deleted, please contact Ch1llDev via email as seen above. You can also join <a href="https://discord.gg/PPtzT8Mu3h">Ch1llDev\'s Discord server</a>, and request your data to be deleted.'
+    text: 'DisPing stores your data until it is no longer needed. If you would like your data to be deleted, please contact Ch1llDev via email as seen above. You can also join <a href="https://discord.gg/PPtzT8Mu3h">Ch1llDev\'s Discord server</a>, and request your data to be deleted.'
 }];
-
-export const metadata = {
-    title: "Privacy Policy",
-    description: "Read about how we use your data.",
-};
 
 export default function Render() {
     return (
         <>
             <Navbar />
             <Heading className={"display-hb font-bold text-left"} style={{ width: "100%", fontSize: "35px", color: "rgb(255, 255, 255, .85)" }}>
-                📜 Terms of Service
+                📜 Privacy Policy
             </Heading>
             <p style={{ width: "100%", fontSize: "15px", textAlign: "left", color: "var(--DisPing-light)" }}>Last updated November 19th, 2022</p>
             {terms.map((term, number) => {
